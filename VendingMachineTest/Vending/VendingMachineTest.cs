eefcc9c1ca0d7cb791c189d7df2ef603801892bc
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-06-17 19:58:56 +0900

    微調整。ネームスペースをテスト対象と合わせる。

--- VendingMachineTest/Vending/VendingMachineTest.cs
+++ VendingMachineTest/Vending/VendingMachineTest.cs
@@ -3,7 +3,7 @@ using System;
 using TddbcCSharp.Vending;
 using System.Collections.Generic;
 
-namespace TddbcCSharpNUnit
+namespace TddbcCSharp.Vending
 {
 	[TestFixture()]
 	public class VendingMachineTest
