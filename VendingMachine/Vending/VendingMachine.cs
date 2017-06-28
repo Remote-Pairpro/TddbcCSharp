f289f9f88d9852c79e1de16168f15e2904ba54b7
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-06-28 16:17:23 +0900

    初期化。いろいろご破算。テストが実行できる状況だけを作る。

--- VendingMachine/Vending/VendingMachine.cs
+++ VendingMachine/Vending/VendingMachine.cs
@@ -5,57 +5,6 @@ namespace TddbcCSharp.Vending
 {
     public class VendingMachine
     {
-
-        int _totalAmount = 0;
-        DrinkKindAndStocks _drinkKindAndStocks;
-
-        public VendingMachine()
-        {
-            _drinkKindAndStocks = new DrinkKindAndStocks();
-        }
-
-        public int TotalAmount
-        {
-            get { return _totalAmount; }
-        }
-
-        public void Insert(int amount)
-        {
-            _totalAmount += amount;
-        }
-
-        public int PayBack()
-        {
-            int lastTotal = _totalAmount;
-            _totalAmount = 0;
-            return lastTotal;
-            ;
-        }
-
-        public void ClearStocks()
-        {
-            _drinkKindAndStocks.Clear();
-        }
-
-        public int CountKinds()
-        {
-            return _drinkKindAndStocks.CountKinds();
-        }
-
-        public void AddDrinkKind(DrinkKind drink, int stockCount)
-        {
-            _drinkKindAndStocks.AddDrinkKind(drink, stockCount);
-        }
-
-        public List<DrinkKind> AllDrinkKinds()
-        {
-            return _drinkKindAndStocks.AllDrinkKinds();
-        }
-
-        public int CountStockOf(DrinkKind drinkKind)
-        {
-            return _drinkKindAndStocks.CountStockOf(drinkKind);
-        }
     }
 }
 
