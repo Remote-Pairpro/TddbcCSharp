51dda3ee25fca9b0ef37ebc000288db491469f70
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-07-06 02:47:10 +0900

    MoqTest added.

---
+++ VendingMachineTest/Vending/MoqTest.cs
@@ -0,0 +1,46 @@
+﻿using Moq.Protected;
+using Moq;
+using NUnit.Framework;
+using static VendingMachine.Vending.JapaneseMoney;
+using static VendingMachine.Vending.JapaneseMoneySupport;
+
+namespace VendingMachineTest.Vending
+{
+
+    public class CTest
+    {
+        //        public string TestTest(string test)
+        //        {
+        //            return "abc";
+        //        }
+
+        public virtual string TestTest(string test) {
+            return "";
+        }
+
+        public string Foo(string test)
+        {
+            return this.TestTest(test);
+        }
+
+    }
+
+    [TestFixture]
+    public class MoqTest
+    {
+
+
+        [Test]
+        public void Moqtesuto()
+        {
+            Mock<CTest> m = new Mock<CTest> { CallBase = true };
+//            Mock<CTest> mockT = Mock.Get<CTest>(m.Object);
+//            m.Setup(mtt => mtt.TestTest("koregahairuyotei")).Returns("yamada");
+            m.Setup(mtt => mtt.TestTest("koregahairuyotei")).Returns("yamada");
+
+            string actual = m.Object.Foo("koregahairuyotei");
+
+            Assert.AreEqual("yamada", actual);
+        }
+    }
+}
